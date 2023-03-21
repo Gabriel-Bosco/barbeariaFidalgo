3ba8c64a1c0aa2ea0672c4ccd3d8c2be8211ff90
Gabriel Bosco <gabrielboscodev@gmail.com>
2023-03-21 17:16:18 -0300

    rolagem suave, menu fixo, correção na criação da galeria

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+/* FIXA MENU */
+const nav = document.querySelector('nav');
+const logo = document.querySelector('.logo')
+
+window.addEventListener('scroll', function() {
+  // obtém a posição da janela em relação ao topo da página
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop >= 100) {
+    nav.classList.add('fixo', "mostrar");
+    logo.classList.add('reduz')
+  } else {
+    nav.classList.remove('fixo', "mostrar");
+    logo.classList.remove('reduz')
+  }
+});
+
+
+/* ROLAGEM SUAVE */
+var menuLinks = document.querySelectorAll('.menu-item');
+
+menuLinks.forEach(function(menuLink) {
+  menuLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    var target = document.querySelector(menuLink.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
 /* MAQUINA DE ESCREVER */
 
 const inserir = document.querySelector("#nome")
@@ -44,43 +76,55 @@ function mostranome(inserir, insercao, antenome, nome, intervalo){
 mostranome(inserir, insercao, antenome, nome, intervalo);
 
 
-
-/* CRIA IMAGENS NA GALERIA */
-const frame = []; //array da moldura
-const foto = []; //array das fotos
-let local = document.getElementById("local"); //local da inserção das fotos
-let x = 456; //valor do x da linha inferior
-let x2 = 590; //valor do x da linha superior
-
-window.onload= () => { //funciona simultaneo ao carregamento da pgn
-    for (let c = 1; c <= 5; c++) {
-
+/* CRIAR GALERIA */
+function criarGaleria() {
+    const frame = []; //array da moldura
+    const foto = []; //array das fotos
+    let local = document.getElementById("local"); //local da inserção das fotos
+    let x = 456; //valor do x da linha inferior
+    let x2 = 590; //valor do x da linha superior
+  
+    window.onload = () => {
+      //funciona simultaneo ao carregamento da pgn
+      for (let c = 1; c <= 5; c++) {
         frame[c] = document.createElement("img");
-        frame[c].src = ("Imagens/frame.png");
-        local.appendChild(frame[c])
-        frame[c].classList.add("moldura")
-
+        frame[c].src = "Imagens/frame.png";
+        local.appendChild(frame[c]);
+        frame[c].classList.add("moldura");
+  
         foto[c] = document.createElement("img"); //cria um elemento imagem em cada posicao da array
-        foto[c].src = (`Imagens/foto${c}.png`); //define o link da foto a ser adicionada
-        local.appendChild(foto[c]) //adiciona a imagem ao local
+        foto[c].src = `Imagens/foto${c}.png`; //define o link da foto a ser adicionada
+        local.appendChild(foto[c]); //adiciona a imagem ao local
         foto[c].classList.add("foto"); //adiciona o atributo class as imagens
-
+        foto[c].id = `foto${c}`;
+  
         if (c <= 3) {
-            frame[c].style.top = "1690px"; //define o y da imagem
-            frame[c].style.left = `${x - 17}px`; //define o x da imagem
-
-            foto[c].style.top = "1707px";
-            foto[c].style.left = `${x}px`;
-            x = x + 230; //distancia entre as imagens
-
+          frame[c].style.top = "1690px"; //define o y da imagem
+          frame[c].style.left = `${x - 17}px`; //define o x da imagem
+  
+          foto[c].style.top = "1707px";
+          foto[c].style.left = `${x}px`;
+          x = x + 230; //distancia entre as imagens
+        } else {
+          frame[c].style.top = "1395px";
+          frame[c].style.left = `${x2 - 17}px`;
+  
+          foto[c].style.top = "1412px";
+          foto[c].style.left = `${x2}px`;
+          x2 = x2 + 230;
         }
-        else {
-            frame[c].style.top = "1395px";
-            frame[c].style.left = `${x2 - 17}px`;
+        
+        // Adiciona o event listener de clique para cada imagem
+        foto[c].addEventListener("click", function() {
+          console.log(`Você clicou na foto ${c}`);
+          /* criar janela sobre serviços*/
+        });
+      }
+    };
+  }
+  
+criarGaleria();
 
-            foto[c].style.top = "1412px";
-            foto[c].style.left = `${x2}px`;
-            x2 = x2 + 230;
-        }
-    }
-}
+
+
+
